0cf08240525a8627cd47ff2846388c21a33b61f2
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-03-13 15:40:12 -0300

    exs Operadores de comparação concluído

--- desafioSaty/03-comparacao.js
+++ desafioSaty/03-comparacao.js
@@ -15,3 +15,36 @@ console.log(5 __ 5);
 console.log('5' __ 5);
 console.log(userAge __ 18); // a idade do usuário precisa ser maior ou igual a 18 para poder abrir uma conta
 console.log(userHeight __ 120); // a altura do usuário precisa ser menor ou igual a 120cm para poder ir no brinquedo
+
+
+let userAge=25;
+let userHeight=110;
+console.log (userAge != userHeight);
+
+
+let userAge=21;
+let userHeight=22;
+console.log ( userAge <= userAge);
+
+
+let userAge=5;
+let userHeight=5;
+console.log (userAge === userHeight);
+
+
+let userAge='5';
+let userHeight=5;
+console.log (userAge==userHeight)
+
+let Age=25;
+let userHeight=18;
+console.log(Age>=userHeight)
+
+let userHeight=120;
+let height=110;
+console.log (userHeight>=height)
+
+
+
+
+
