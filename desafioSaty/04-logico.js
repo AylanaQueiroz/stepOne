5043044b5c9d57a95c168127d79dc45dc8fd6739
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-03-13 17:29:49 -0300

    exercício operadores lógicos concluído

--- desafioSaty/04-logico.js
+++ desafioSaty/04-logico.js
@@ -4,17 +4,22 @@
 // exemplo:
 // se a pessoa é adulta (isAdult) e tem um carro (car), ela pode dirigir (canDrive)
 
-// solução:
+// solução: 
+/*
 let isAdult = true;
 let car = true;
 let canDrive = isAdult && car
-console.log(canDrive); //answer: true;
+console.log(canDrive); //answer: true;*/
 
 
 // 1 - se a pessoa é adulta (isAdult) e tem uma cerveja (beer), ela pode beber se quiser (canDrink)
+/*
+let isAdult = true; 
+let beer = true ;
+let canDrink = isAdult && beer;
 
-
-
+console.log (canDrink); 
+*/
 
 // 2 - se está chovendo (rain) e voce tem um guardachuva (umbrella)
 // Faça uma variável com o nome de molhado (wet)
@@ -23,17 +28,45 @@ console.log(canDrive); //answer: true;
 
 let rain = true;
 let umbrella = true;
-let wet;
+let wet ;
 
 if(rain && umbrella ) {
-    wet = ???;
-} else {
-    wet = ???;
+    wet = false
+}else {
+    wet = true
 }
+
+console.log(wet)
+
+
 
 // 3 - se a pessoa é estudante (student) ou é aposentado (retired),
 // ela pode ter desconto no ingresso do show (discount)
 
+let student=true;
+let retired =true;
+let discunt;
+
+if (student || retired) {
+    discunt = true
+} else {
+    discunt=false
+}
+
+console.log(discunt)
+
 
 // 4 - se a pessoa tem olhos verde (greenEyes) ou olhos azuis (blueEyes),
 // ela tem olhos claros (lightColorEyes)
+
+let greenEyes=true;
+let blueEyes=true;
+let lightColorEyes;
+
+if (greenEyes || blueEyes) {
+    lightColorEyes = true
+} else {
+    lightColorEyes = false
+}
+
+console.log (lightColorEyes)
