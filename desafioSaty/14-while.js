5bacb16b9b3ba866257b577a48a75b55de8fa459
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-03-17 15:43:50 -0300

    exercício while 14 concluído

--- desafioSaty/14-while.js
+++ desafioSaty/14-while.js
@@ -3,3 +3,38 @@
 // Utilizando o while, faça uma estrutura de repetição que dê o console somente dos números pares, começando no 2 e indo até 100.
 
 // Utilizando o while, faça uma estrutura de repetição que dê o console dos números 100 a 1.
+
+/*
+let inicio = 50
+
+while ( inicio <101 & inicio > 49) {
+
+    console.log(inicio);
+
+    inicio++;
+}
+
+
+
+
+let pares = 2
+
+while (pares <= 100) {
+
+    console.log(pares);
+
+    pares =pares +2 ;
+}
+
+*/
+
+
+let inverso = 100;
+
+while (inverso >= 1) {
+
+    console.log(inverso);
+
+    inverso = inverso-1;
+}
+
