95806118e3eabc4461eb74c2c678f417e1d581bd
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-04-10 17:17:52 -0300

    exercício horário concluído

--- exs_guanabara/main.js
+++ exs_guanabara/main.js
@@ -1,1 +1,9 @@
-alert ('ola')
+function carregar () {
+    var msg= document.querySelector('.msgm')
+    var img= document.querySelector('.imgm')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML= "Agora são " + hora +" horas"
+    
+
+}
