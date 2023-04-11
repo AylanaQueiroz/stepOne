4d8bd378afbaf3f84ae180d80ae676c77fea24f4
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-04-11 16:25:19 -0300

    PROJETO: PROGRAMA QUE INFORMA O HORÁRIO ATUAL

--- exs_guanabara/main.js
+++ exs_guanabara/main.js
@@ -3,21 +3,28 @@ function carregar() {
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 10
+    //var hora = 22
     msg.innerHTML = "Agora são " + hora + " horas"
 
 
     if (hora >= 0 && hora < 12) {
 
         img.src ='imagens/manha.jpg'
+        document.body.style.background='green'
+        msg.innerHTML = 'Bom dia!' + " Agora são " + hora + " horas"
+        
 
     } else if (hora >= 12 && hora < 18) {
 
        img.src ='imagens/tarde.jpg'
+       document.body.style.background='brown'
+       msg.innerHTML = 'Boa tarde!' + " Agora são " + hora + " horas" 
 
     } else {
 
         img.src ='imagens/noite.jpg'
+        document.body.style.background='gray'
+        msg.innerHTML = 'Boa noite!' + " Agora são " + hora + " horas"
     }
 
 }
