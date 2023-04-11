19172bba6b15d4ff1326b722f75c4a6206bd0770
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-04-11 15:37:36 -0300

    exercício horário concluído

--- exs_guanabara/main.js
+++ exs_guanabara/main.js
@@ -1,9 +1,23 @@
-function carregar () {
-    var msg= document.querySelector('.msgm')
-    var img= document.querySelector('.imgm')
+function carregar() {
+    var msg = document.querySelector('#msg')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML= "Agora são " + hora +" horas"
-    
+    //var hora = 10
+    msg.innerHTML = "Agora são " + hora + " horas"
+
+
+    if (hora >= 0 && hora < 12) {
+
+        img.src ='imagens/manha.jpg'
+
+    } else if (hora >= 12 && hora < 18) {
+
+       img.src ='imagens/tarde.jpg'
+
+    } else {
+
+        img.src ='imagens/noite.jpg'
+    }
 
 }
